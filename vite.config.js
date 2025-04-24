638d9c82ecf4c5b538cf9baf1919493bbede1f10
Thapoojaa <thapoojaathavanesan@gmail.com>
2025-04-24 16:20:23 +0530

    Fix: Set base to './' for relative paths

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/TaleRouterDom/', // Set this to your deployment path
   plugins: [react()],
-  base: '/TaleRouterDom/', // Replace with your repository name
 });
